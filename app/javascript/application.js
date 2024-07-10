005eaa0c428f4107efc37c30897fce1274b5d7e6
Junaid Ansari <junaid@enertia.world>
2024-07-10 23:20:13 +0530

    add flash messasage with Hotwire

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Entry point for the build script in your package.json
-// import "@hotwired/turbo-rails"
-// import "./controllers"
-
+import "@hotwired/turbo-rails"
 import "./controllers"
 
-// The two following lines disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+// import "./controllers"
+
+// // The two following lines disable Turbo on the whole application
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
