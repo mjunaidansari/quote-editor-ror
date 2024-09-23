696e850d27394ef4d77d83a2008d4ef43d2286fb
Junaid Ansari <junaid@enertia.world>
2024-09-23 13:21:26 +0530

    Modify routes for TwoFactorAuth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +12,14 @@ Rails.application.routes.draw do
   resources :quotes do
     resource :line_item_dates, except: [:index, :show]
   end
+
+  resources :two_factor_auth, only: [:show] do
+    post :enable, on: collection
+    post :disable, on: collection
+  end
+
+  get 'users/verify_otp', to: 'users/sessions#verify_otp', as: :verify_otp
+  get 'users/verify_otp', to: 'users/sessions#check_otp'
 
   # Defines the root path route ("/")
   # root "posts#index"
