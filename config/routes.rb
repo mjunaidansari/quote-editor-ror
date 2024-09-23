417d83d3ca2694fb7664d44979fc30b37f420e6c
Junaid Ansari <junaid@enertia.world>
2024-09-23 22:23:53 +0530

    Fix TwoFactorAuth show page to work wihtout needing id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  devise_scope :user do
+    get 'users/verify_otp', to: 'users/sessions#verify_otp', as: :verify_otp
+    post 'users/verify_otp', to: 'users/sessions#check_otp'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,13 +20,15 @@ Rails.application.routes.draw do
     resource :line_item_dates, except: [:index, :show]
   end
 
-  resources :two_factor_auth, only: [:show] do
-    post :enable, on: collection
-    post :disable, on: collection
+  resources :two_factor_auth do
+    collection do
+      get :show
+      post :enable
+      post :disable
+    end
   end
 
-  get 'users/verify_otp', to: 'users/sessions#verify_otp', as: :verify_otp
-  get 'users/verify_otp', to: 'users/sessions#check_otp'
+
 
   # Defines the root path route ("/")
   # root "posts#index"
